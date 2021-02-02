a50d095031fde93a2bfe6b0c4eecffc7c09ba4db
matteobuscicchio <matteoezio@me.com>
2021-02-02 12:40:26 +0100

    aggiunte direttive esercizio

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+    // Stampare a schermo una decina di dischi musicali (vedi screenshot).
+    // Utilizzare:
+    // Html, Sass, JS, VueJS, PHP
+    // Prima Milestone:
+    // Stampiamo i dischi solo con l’utilizzo di PHP, che stampa direttamente i dischi in pagina:
+    //  al caricamento della pagina ci saranno tutti i dischi.
+    // Seconda Milestone:
+    // Attraverso l’utilizzo di AJAX: al caricamento della pagina 
+    // ajax chiederà attraverso una chiamata i dischi a php e li stamperà attraverso VueJS.
+    // Bonus:
+    // Attraverso un’altra chiamata ajax, filtrare gli album per artista
+    // Lo screenshot si trova nelle slide.
+
+?>
